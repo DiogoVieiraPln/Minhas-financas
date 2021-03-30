5a30d6e1f8eec072fbeb1c7158f7f1c0665ea9de
diogo vieira <diogovieira341di@gmail.com>
2021-03-30 19:38:28 -0300

    -1erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Dashboard from './pages/Dashboard';
-import { initialData } from './data/initialData';
+
 
 import './styles/global.css';
 import TransactionsProvider from './context/TransactionsContext';
